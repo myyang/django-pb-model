34448e94cf509e06425b9fd99eda0bd34e350ec2
myyang <ymy1019@gmail.com>
2017-11-28 22:27:26 +0800

    pypi: update version info

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pb-model',
-    version='v0.1.3',
+    version='v0.1.4',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
