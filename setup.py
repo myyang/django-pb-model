fec26ce6687520c809e47fe77e26042b05d2bba7
Meng-Ying Yang <ymy1019@gmail.com>
2018-06-07 10:12:48 +0800

    Update package information.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pb-model',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -26,7 +26,7 @@ setup(
     author='myyang',
     author_email='ymy1019@gmail.com',
     install_requires=[
-        'django>=1.8',
+        'django>=1.8,<1.11',
         'protobuf>=3.1',
     ],
     classifiers=[
