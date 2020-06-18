0a7e29220db30bebc345681e4e1a217a61324bcd
Meng-Ying Yang <ymy1019@gmail.com>
2020-06-18 14:20:41 +0800

    root: release 0.1.10

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pb-model',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
